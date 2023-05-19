30745e57f79af0a398acf4429e646d2b32171c14
VCAngel <vcangel00@gmail.com>
2023-05-19 12:58:48 -0600

    feature: Header styles with tailwindcss

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,5 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent {
   logo = './assets/svg/logo.svg';
+  @Input() isLanding = true;
 }
